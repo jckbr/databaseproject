155515ddbe2da93d4b51cf52abdffc9b64c21033
jck <bear@ltgi.net>
2021-03-18 13:20:53 -0400

    Finished random generators, need to implement into DatabaseUI.java.

--- appSrc/src/GenerateRandom.java
+++ appSrc/src/GenerateRandom.java
@@ -38,6 +38,65 @@ public class GenerateRandom
         return names.get(random1) + " " + names.get(random2);
     }
 
+    public String generateRandomGenre()
+    {
+        HashMap<Integer, String> genres = new HashMap<Integer, String>();
+        int count = 0;
+
+        try
+        {
+            File dict = new File("genres.txt");
+            Scanner readGenres = new Scanner(dict);
+
+            while(readGenres.hasNextLine())
+            {
+                genres.put(count++, readGenres.nextLine());
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File not found. Make sure file 'genres.txt' is in src folder.");
+        }
+
+        int random = (int) (Math.random() * count);
+
+        return genres.get(random);
+    }
+
+    public int generateRandomPay()
+    {
+
+        return (int) (Math.random() * 100000);
+    }
+
+    public String generateRandomDate()
+    {
+        int month = (int) (Math.random() * 11) + 1;
+        int day = (int) (Math.random() * 28) + 1;
+        int year = (int) (Math.random() * 2019) + 1;
+
+        return month + "-" + day + "-" + year;
+    }
+
+    public String generateRandomRegion()
+    {
+        HashMap<Integer, String> regions = new HashMap<Integer, String>();
+        regions.put(0, "North");
+        regions.put(1, "East");
+        regions.put(2, "South");
+        regions.put(3, "West");
+
+        int random = (int) (Math.random() * 3) + 1;
+
+        return regions.get(random);
+    }
+
+    public int generateRandomPrice()
+    {
+
+        return (int) (Math.random() * 60);
+    }
+
     public String generateRandomGame()
     {
         HashMap<Integer, String> dictionary = new HashMap<Integer, String>();
